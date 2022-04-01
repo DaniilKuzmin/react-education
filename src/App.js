19416e53cfc5a04e121c4da12ebaa9766f38133f
Daniil <nicksores@gmail.com>
2022-04-02 00:49:00 +0300

    lesson_2 hw

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import './App.css';
-//import React from "react";
-import {useState} from "react";
-    
-
+import React, {useState, useEffect} from "react";
+import Message from './Message.js'
+import AUTHOR from './constants/common.js'
 
 function App(props) {
 
-const [count, setCount] = useState(0); 
+const [messageList, setMessageList] = useState([]);
 const [value, setValue] = useState('');
 
+useEffect(() => {
+  if (messageList.length > 0) {
+    if (messageList[messageList.length - 1].author === AUTHOR.me) {
+        setTimeout(() => {
+          let messageObj = {text: 'Some robot text', author: AUTHOR.bot}
+
+          setMessageList([...messageList, messageObj])
+        }, 1500)
+    }
+  }
+}, [messageList])
+
+const handleChange = (event) => {
+  setValue(event.target.value)
+}
+
+const handler = () => {
+
+  let messageObj = {text: value, author: AUTHOR.me}
+
+  setMessageList([...messageList, messageObj])
+
+}
 
   return (
     <div className="App">
       <header className="App-header">
-
-        <input type="text" placeholder="Введите сообщение" /> 
-        <button>Отправить</button>
-
+        <h4>Список сообщений:</h4>
+        <br/>
+        {messageList.map((element, index) => (<Message text={element} key={index} />))}
+        <div>
+          <input type="text" placeholder="Введите сообщение" onChange={handleChange}/> 
+          <button onClick={handler}>Отправить</button>
+        </div>
       </header>
     </div>
   );
